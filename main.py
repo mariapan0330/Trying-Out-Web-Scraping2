c039834127cb327ba2c8bda18aa016ce4bf91a23
Maria Panagos <maria.pan0330@gmail.com>
2023-04-12 12:56:56 -0500

    changed a few things and tested it out

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://example.com'  # replace with the URL of the website you want to extract HTML from
+url = 'https://en.wikipedia.org/wiki/Parakeet'
 response = requests.get(url)
 html_content = response.text
+print(response)
 soup = BeautifulSoup(html_content, 'html.parser')
+# print(soup.prettify())
 
 # find the job title and company name elements using Beautiful Soup's selector syntax
-job_title = soup.select_one('.job-title').text
-company_name = soup.select_one('.company-name').text
+title = soup.find(class_='mw-page-title-main')
+print(title)
 
 # use the job title and company name in your app
